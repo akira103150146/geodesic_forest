f2f8c2c5f43fc5255e01adb20f189412c8d9e3e5
Akira <sunroad0418@universetech.cc>
2020-12-10 00:05:53 +0800

    feat: similarity matrix

--- main.py
+++ main.py
@@ -1,8 +1,89 @@
 import numpy as np
 import tree as tr
 
-with open('test.csv', 'r', newline='') as csv:
-    lines=csv.readlines()
+
+def choose_n_points(mat, number: int):
+    indexes = np.random.randint(0, mat.shape[0], size=number)
+
+    return indexes - 1, mat[indexes]
+
+
+def create_point_array(choosePoints):
+    points = []
+
+    for p in choosePoints:
+        points.append(tr.point(p[1:4], p[0]))
+
+    return np.array(points)
+
+
+def create_n_trees(data: np.array, number: int):
+    trees = []
+
+    for i in range(number):
+        indexes, choosePoints = choose_n_points(data, 20)
+        root = tr.treeNode(create_point_array(choosePoints), indexes=indexes)
+        tr.build_tree(root, 3)
+        trees.append(root)
+
+    return trees
+
+
+def get_point_appear_in_trees_count(trees, index):
+    count = 0
+
+    for tree in trees:
+        if index in tree.indexes:
+            count += 1
+
+    return count
+
+def get_all_points_groups(trees):
+    groups = []
+    for tree in trees:
+        tree.getPointsGroup(groups)
+
+    return groups
+
+def get_similarity_matrix(trees, indexes):
+    mat = np.zeros((1000, 1000))
+    groups = get_all_points_groups(trees)
+
+    for x in indexes:
+        for y in indexes:
+            if x == y:
+                mat[int(x), int(y)] = 1
+
+    uniqueElements = []
+
+    for group in groups:
+        for element in group:
+            if not element in uniqueElements:
+                uniqueElements.append(element)
+
+    for element in uniqueElements:
+        t = get_point_appear_in_trees_count(trees, element)
+        needToAdds = {}
+
+        for group in groups:
+            if element in group:
+                for index in group:
+                    if index != element:
+                        if needToAdds.get((element, index)) is None:
+                            needToAdds[(element, index)] = 1
+                        else:
+                            needToAdds[(element, index)] += 1
+
+        for key in needToAdds:
+            print(key)
+            mat[key] = needToAdds[key] / t
+
+    np.savetxt('similarity_matrix.txt', mat, fmt='%.2f')
+
+with open('linear.csv', 'r', newline='') as csv:
+    np.set_printoptions(suppress=True)
+    lines = csv.readlines()
     data = np.genfromtxt(lines, delimiter=',')
-    root = tr.treeNode(data)
-    tr.build_tree(root, data.shape[1])
+    trees = create_n_trees(data, 5)
+    # [print(t.printTree()) for t in trees]
+    get_similarity_matrix(trees, data[1:,0])
--- tree.py
+++ tree.py
@@ -1,42 +1,66 @@
 import numpy as np
 import fast_bic as fb
 
-class treeNode:  
-    def __init__(self, value: np.array, depth = 1):
+
+class point:
+    def __init__(self, value: np.array, index: int):
+        self.value = value
+        self.index = index
+
+    def getValue(self):
+        return self.value
+
+class treeNode:
+    def __init__(self, value: np.array, depth=1, indexes=None):
         self.value = value
         self.depth = depth
         self.left = None
         self.right = None
+        self.indexes = indexes
 
-    # def __str__(self):
-    #     if self.value is None:
-    #         return "Node: \n"
-        
-    #     result = ''
-    #     for e in self.value:
-    #         result = result + str(e)
-    #     return result + "\n" + "\tL:" + self.left.__str__() + "\n" + "\tR:" + self.right.__str__() + "\n"
-    
     def getColumnByDim(self, dim: int):
-        column = self.value[:,dim]
+        column = [p.value[dim] for p in self.value]
         column.sort()
 
         return column
 
     def count(self):
         return len(self.value)
-    
+
     def split(self, dim: int, splitPoint: float):
-        sortedMatrix = self.value[np.argsort(self.value[:, dim])]
-        self.left = treeNode(sortedMatrix[sortedMatrix[:, dim] <= splitPoint], self.depth + 1)
-        self.right = treeNode(sortedMatrix[sortedMatrix[:, dim] > splitPoint], self.depth + 1)
-        print("split" + " depth: " + str(self.depth) + " dim: " + str(dim) + "  splitPoint: " + str(splitPoint))
-        print("left: ", self.left.value)
-        print("right: ", self.right.value)
-        print("------------")
+        sortedPoints = sorted(self.value, key=lambda p: p.value[dim])
+        left = [x for x in sortedPoints if x.value[dim] <= splitPoint]
+        right = [x for x in sortedPoints if x.value[dim] > splitPoint]
+        self.value = None
+        self.left = treeNode(left, depth=self.depth + 1)
+        self.right = treeNode(right, depth=self.depth + 1)
+
+
+    def printTree(self):
+        if self.value != None:
+            [print("index: ", p.index, " value: ", p.value) for p in self.value]
+            print("\n----")
+
+        if self.left != None:
+            self.left.printTree()
+
+        if self.right != None:
+            self.right.printTree()
+
+    def getPointsGroup(self, result):
+        if self.value != None:
+            result.append([int(v.index) for v in self.value])
+
+        if self.left != None:
+            self.left.getPointsGroup(result)
+
+        if self.right != None:
+            self.right.getPointsGroup(result)
+
 
 def should_split_continue(tree: treeNode):
-    return tree.count() >= 5 
+    return tree.count() >= 5
+
 
 def build_tree(tree: treeNode, d: int):
     if not should_split_continue(tree):
@@ -46,25 +70,18 @@ def build_tree(tree: treeNode, d: int):
         tree.split(bestDim, splitPoint)
         build_tree(tree.right, d)
         build_tree(tree.left, d)
-        
-def find_best_dim_and_split_point(tree: treeNode, d: int):
+
+
+def find_best_dim_and_split_point(node: treeNode, d: int):
     tempBestDim = None
     tempSplitPoint = None
     tempMinBIC = np.Inf
 
     for i in range(d):
-        splitPoint, minBIC = fb.fast_bic(tree.getColumnByDim(i))
+        splitPoint, minBIC = fb.fast_bic(node.getColumnByDim(i))
         if minBIC < tempMinBIC:
             tempBestDim = i
             tempSplitPoint = splitPoint
-    
+            tempMinBIC = minBIC
+
     return tempBestDim, tempSplitPoint
-
-
-
-
-
-
-
-       
-    
